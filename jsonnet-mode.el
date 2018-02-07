319a82f8717835c9f9ea5020d5f2adcbd5894da8
Benjamin Staffin <benley@gmail.com>
2018-02-07 17:45:56 -0500

    Add .libsonnet filetype to auto-mode-alist

--- jsonnet-mode.el
+++ jsonnet-mode.el
@@ -235,6 +235,7 @@ If not inside of a multiline string, return nil."
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist (cons "\\.jsonnet\\'" 'jsonnet-mode))
+(add-to-list 'auto-mode-alist (cons "\\.libsonnet\\'" 'jsonnet-mode))
 
 ;; Utilities for evaluating and jumping around Jsonnet code.
 ;;;###autoload
