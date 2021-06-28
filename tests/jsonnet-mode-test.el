2118613651889c609f94d0e73f584400d861cc65
Thomas Minor <minort@gmail.com>
2021-06-28 14:39:15 -0400

    Add basic Coveralls/undercover configuration

--- tests/jsonnet-mode-test.el
+++ tests/jsonnet-mode-test.el
@@ -1,9 +1,14 @@
 ;;; jsonnet-mode-test.el --- Jsonnet Mode Tests -*- lexical-binding: t -*-
 
-;; (when (require 'undercover nil t)
-;;   (undercover "*.el"))
+(when (require 'undercover nil t)
+  (let ((undercover-force-coverage t))
+    (undercover "*.el" "tests/*.el"
+                (:report-file "./coverage-final.json")
+                (:send-report nil)
+                (:report-format 'coveralls))))
 
 (require 'jsonnet-mode)
+(require 'undercover)
 
 (progn (require 'f)
        (add-to-list 'load-path (f-parent (f-parent (f-this-file))))
